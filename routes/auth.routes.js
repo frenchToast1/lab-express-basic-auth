ac59b286ab8e359e5779fce3f3698a4df6619dd3
frenchToast <juliekogan1@gmail.com>
2021-11-25 21:19:06 +0100

    iterartion2 again

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,24 +10,38 @@ const User = require("../models/User.model.js");
 
 // .post() route ==> to process form data
 router.post("/signup", (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { username, email, password } = req.body; // {username: 'jdoe@gmail.com', password: 'toto123'}
 
   const hashedPassword = bcryptjs.hashSync(password, salt);
   console.log(`Password hash: ${hashedPassword}`);
 
   res.send("ok");
 
-  // Creer un nouveal utilisateur en DB
-  User.create({
-    username: "juliekogan1@gmail.com",
-    password: "123456789",
-  })
+  router.post("/login", (req, res, next) => {
+    const { username, password } = req.body;
+    if (username === "" || password === "") {
+      res.render("auth/login", {
+        errorMessage: "Please enter both, username and password to login.",
+      });
+      return;
+    }
 
-    .then((userFromDB) => {
-      console.log("Newly created user is: ", userFromDB);
-    })
-    .catch((error) => next(error));
+    User.findOne({ username }) // <== check if there's user with the provided username
+      .then() // response from DB - doesn't matter if found or not)
+      .catch(); // error handler in case some error occurred while getting the data from the DB)
+  });
 });
+
+// Creer un nouveal utilisateur en DB
+User.create({
+  username: req.body.username,
+  password: req.body.password,
+})
+
+  .then((userFromDB) => {
+    console.log("Newly created user is: ", userFromDB);
+  })
+  .catch((error) => next(error));
 
 //
 module.exports = router;
